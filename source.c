b794423a3312aaceb1e3ffcc8d4bd3da7e88d390
LKerr42 <LiamKerr42@outlook.com>
2025-10-21 20:26:44 +1300

    Figuring out rectangle rendering, got a blue wireframe square

--- source.c
+++ source.c
@@ -10,8 +10,7 @@ static SDL_Renderer *renderer = NULL;
 #define WINDOW_HEIGHT 480
 
 /* This function runs once at startup. */
-SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
-{
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
     SDL_SetAppMetadata("Play Tetis!", "0.1", "com.github.SDL-Tetris.LKerr42");
 
     if (!SDL_Init(SDL_INIT_VIDEO)) {
@@ -28,8 +27,7 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 }
 
 /* This function runs when a new event (mouse input, keypresses, etc) occurs. */
-SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
-{
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
     if (event->type == SDL_EVENT_QUIT) {
         return SDL_APP_SUCCESS;  /* end the program, reporting success to the OS. */
     }
@@ -37,50 +35,62 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 }
 
 /* This function runs once per frame, and is the heart of the program. */
-SDL_AppResult SDL_AppIterate(void *appstate)
-{
+SDL_AppResult SDL_AppIterate(void *appstate) {
+    SDL_FRect rects[5];
+    const Uint64 rn = SDL_GetTicks();
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 255, SDL_ALPHA_OPAQUE); //set colour then dimentions
+    rects[0].x = (WINDOW_WIDTH / 2) - 100;
+    rects[0].y = (WINDOW_HEIGHT / 2) - 100;
+    rects[0].w = rects[0].h = 100;
+
+    SDL_RenderRect(renderer, &rects[0]); // render rectangle
+
+    SDL_RenderPresent(renderer); // render everything
+
+    /*
     SDL_FRect rects[16];
     const Uint64 now = SDL_GetTicks();
     int i;
 
-    /* we'll have the rectangles grow and shrink over a few seconds. */
+    //we'll have the rectangles grow and shrink over a few seconds.
     const float direction = ((now % 2000) >= 1000) ? 1.0f : -1.0f;
     const float scale = ((float) (((int) (now % 1000)) - 500) / 500.0f) * direction;
 
-    /* as you can see from this, rendering draws over whatever was drawn before it. */
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);  /* black, full alpha */
-    SDL_RenderClear(renderer);  /* start with a blank canvas. */
+    //as you can see from this, rendering draws over whatever was drawn before it.
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);  //black, full alpha
+    SDL_RenderClear(renderer);  //start with a blank canvas.
 
-    /* Rectangles are comprised of set of X and Y coordinates, plus width and
-       height. (0, 0) is the top left of the window, and larger numbers go
-       down and to the right. This isn't how geometry works, but this is
-       pretty standard in 2D graphics. */
+    // Rectangles are comprised of set of X and Y coordinates, plus width and
+    // height. (0, 0) is the top left of the window, and larger numbers go
+    // down and to the right. This isn't how geometry works, but this is
+    // pretty standard in 2D graphics.
 
-    /* Let's draw a single rectangle (square, really). */
+    // Let's draw a single rectangle (square, really).
     rects[0].x = rects[0].y = 100;
     rects[0].w = rects[0].h = 100 + (100 * scale);
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, SDL_ALPHA_OPAQUE);  /* red, full alpha */
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, SDL_ALPHA_OPAQUE);  // red, full alpha
     SDL_RenderRect(renderer, &rects[0]);
 
-    /* Now let's draw several rectangles with one function call. */
+    / Now let's draw several rectangles with one function call.
     for (i = 0; i < 3; i++) {
         const float size = (i+1) * 50.0f;
         rects[i].w = rects[i].h = size + (size * scale);
-        rects[i].x = (WINDOW_WIDTH - rects[i].w) / 2;  /* center it. */
-        rects[i].y = (WINDOW_HEIGHT - rects[i].h) / 2;  /* center it. */
+        rects[i].x = (WINDOW_WIDTH - rects[i].w) / 2;  // center it.
+        rects[i].y = (WINDOW_HEIGHT - rects[i].h) / 2;  // center it.
     }
-    SDL_SetRenderDrawColor(renderer, 0, 255, 0, SDL_ALPHA_OPAQUE);  /* green, full alpha */
-    SDL_RenderRects(renderer, rects, 3);  /* draw three rectangles at once */
+    SDL_SetRenderDrawColor(renderer, 0, 255, 0, SDL_ALPHA_OPAQUE);  // green, full alpha 
+    SDL_RenderRects(renderer, rects, 3);  // draw three rectangles at once
 
-    /* those were rectangle _outlines_, really. You can also draw _filled_ rectangles! */
+    // those were rectangle _outlines_, really. You can also draw _filled_ rectangles! 
     rects[0].x = 400;
     rects[0].y = 50;
     rects[0].w = 100 + (100 * scale);
     rects[0].h = 50 + (50 * scale);
-    SDL_SetRenderDrawColor(renderer, 0, 0, 255, SDL_ALPHA_OPAQUE);  /* blue, full alpha */
+    SDL_SetRenderDrawColor(renderer, 0, 0, 255, SDL_ALPHA_OPAQUE);  // blue, full alpha
     SDL_RenderFillRect(renderer, &rects[0]);
 
-    /* ...and also fill a bunch of rectangles at once... */
+    // ...and also fill a bunch of rectangles at once...
     for (i = 0; i < SDL_arraysize(rects); i++) {
         const float w = (float) (WINDOW_WIDTH / SDL_arraysize(rects));
         const float h = i * 8.0f;
@@ -89,17 +99,16 @@ SDL_AppResult SDL_AppIterate(void *appstate)
         rects[i].w = w;
         rects[i].h = h;
     }
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);  /* white, full alpha */
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);  // white, full alpha
     SDL_RenderFillRects(renderer, rects, SDL_arraysize(rects));
 
-    SDL_RenderPresent(renderer);  /* put it all on the screen! */
+    SDL_RenderPresent(renderer);  // put it all on the screen! */
 
-    return SDL_APP_CONTINUE;  /* carry on with the program! */
+    return SDL_APP_CONTINUE;  // carry on with the program!
 }
 
 /* This function runs once at shutdown. */
-void SDL_AppQuit(void *appstate, SDL_AppResult result)
-{
+void SDL_AppQuit(void *appstate, SDL_AppResult result) {
     /* SDL will clean up the window/renderer for us. */
 }
 
