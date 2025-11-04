3f50f1f5a8427b87cc6c1dfba39ff7e8fbedde74
LKerr42 <LiamKerr42@outlook.com>
2025-11-04 21:11:45 +1300

    Added the hold function

--- source.c
+++ source.c
@@ -25,6 +25,7 @@ bool falling = true, winning = false, titleCard = true;
 char scoreString[7] = "0000000";
 
 TTF_Font* globalFont;
+TTF_Font* globalFontS;
 
 // Idea: each block in a Tetromino is a different "object" within this struct, 
 // each with a different starting position and all with one colour.
@@ -97,44 +98,45 @@ void prependChar(char *str, char c) {
 
 void setupStaticText();
 
+int shapes[7][4][4] = {
+    //Long boy
+    {{0, 0, 0, 0}, 
+     {1, 1, 1, 1},
+     {0, 0, 0, 0},
+     {0, 0, 0, 0}},
+    //Left L
+    {{1, 0, 0, 0},
+     {1, 1, 1, 0},
+     {0, 0, 0, 0},
+     {0, 0, 0, 0}},
+    //Right L
+    {{0, 0, 1, 0},
+     {1, 1, 1, 0},
+     {0, 0, 0, 0},
+     {0, 0, 0, 0}},
+    //Square
+    {{1, 1, 0, 0},
+     {1, 1, 0, 0},
+     {0, 0, 0, 0},
+     {0, 0, 0, 0}},
+    //Right squiggle
+    {{0, 1, 1, 0},
+     {1, 1, 0, 0},
+     {0, 0, 0, 0},
+     {0, 0, 0, 0}},
+    //T boy
+    {{0, 1, 0, 0},
+     {1, 1, 1, 0},
+     {0, 0, 0, 0},
+     {0, 0, 0, 0}},
+    //Left squiggle
+    {{1, 1, 0, 0},
+     {0, 1, 1, 0},
+     {0, 0, 0, 0},
+     {0, 0, 0, 0}},
+};
+
 void setupTetrominos() {
-    int shapes[7][4][4] = {
-        //Long boy
-        {{0, 0, 0, 0}, 
-         {1, 1, 1, 1},
-         {0, 0, 0, 0},
-         {0, 0, 0, 0}},
-        //Left L
-        {{1, 0, 0, 0},
-         {1, 1, 1, 0},
-         {0, 0, 0, 0},
-         {0, 0, 0, 0}},
-        //Right L
-        {{0, 0, 1, 0},
-         {1, 1, 1, 0},
-         {0, 0, 0, 0},
-         {0, 0, 0, 0}},
-        //Square
-        {{1, 1, 0, 0},
-         {1, 1, 0, 0},
-         {0, 0, 0, 0},
-         {0, 0, 0, 0}},
-        //Right squiggle
-        {{0, 1, 1, 0},
-         {1, 1, 0, 0},
-         {0, 0, 0, 0},
-         {0, 0, 0, 0}},
-        //T boy
-        {{0, 1, 0, 0},
-         {1, 1, 1, 0},
-         {0, 0, 0, 0},
-         {0, 0, 0, 0}},
-        //Left squiggle
-        {{1, 1, 0, 0},
-         {0, 1, 1, 0},
-         {0, 0, 0, 0},
-         {0, 0, 0, 0}},
-    };
     setTetColour(&tetArray[0], 0, 255, 255); //Long boy
     setTetColour(&tetArray[1], 0, 0, 255);   //Left L
     setTetColour(&tetArray[2], 255, 160, 0); //Right L
@@ -334,6 +336,8 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
             if (i == 0 || i == 21 || j == 0 || j == 11) {
                 filledBlocks[i][j].v = true;
                 filledBlocks[i][j].r = filledBlocks[i][j].g = filledBlocks[i][j].b = 125;
+            } else {
+                filledBlocks[i][j].v = false;
             }
         }
     }
@@ -364,7 +368,12 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
 
     globalFont = TTF_OpenFont("assets\\NES.ttf", 25);
     if (globalFont == NULL) {
-        SDL_Log("Failed to load font: %s", SDL_GetError());
+        SDL_Log("Failed to load font globalFont: %s", SDL_GetError());
+    }
+
+    globalFontS = TTF_OpenFont("assets\\NES.ttf", 17);
+    if (globalFont == NULL) {
+        SDL_Log("Failed to load font globalFontS: %s", SDL_GetError());
     }
 
     setupStaticText();
@@ -527,6 +536,36 @@ void rotateTetrominoCCW(tetromino *t) {
     }
 }
 
+void resetBoard() {
+    for (int i = 0; i < 22; i++) {
+        for (int j = 0; j < 12; j++) {
+            if (i == 0 || i == 21 || j == 0 || j == 11) {
+                filledBlocks[i][j].v = true;
+                filledBlocks[i][j].r = filledBlocks[i][j].g = filledBlocks[i][j].b = 125;
+            } else {
+                filledBlocks[i][j].v = false;
+            }
+        }
+    }
+    buildBoardTexture();
+    renderBoard();
+}
+
+int heldtet = -1;
+
+void resetGame() {
+    resetBoard();
+    for (int i = 0; i < 7; i++) {
+        scoreString[i] = '0';
+    }
+    score = 0;
+    heldtet = -1;
+    currentBlock = 0;
+    tetArray[currentBlock].x = 4;
+    tetArray[currentBlock].y = 1;
+    winning = true;
+}
+
 void handleKeyboardInput(SDL_Scancode event) {
     int indx, furth;
     switch (event) {
@@ -560,11 +599,33 @@ void handleKeyboardInput(SDL_Scancode event) {
             rotateTetrominoCCW(&tetArray[currentBlock]);
             break;
         }
+        case SDL_SCANCODE_R: {
+            if (winning) {
+                resetGame();
+            }
+            break;
+        }
         case SDL_SCANCODE_SPACE: {
             printf("Space pressed\n");
+            if (winning) {
+                //swap values
+                int temp = heldtet;
+                heldtet = currentBlock;
+                currentBlock = temp;
+
+                if (currentBlock == -1) { //if starting
+                    currentBlock = SDL_rand(7);
+                }
+                tetArray[currentBlock].x = 4;
+                tetArray[currentBlock].y = 1;
+            } else {
+                resetGame();
+            }
+
             if (titleCard) {
                 titleCard = false;
                 winning = true;
+                printf("heldtet before: %d", heldtet);
             }
             break;
         }
@@ -598,6 +659,7 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
 }
 
 void moveBoardDown(int remove) {
+    //start at the index and set each to the one above
     for (int i = remove; i >= 2; i--) {
         for (int j = 1; j <= 10; j++) {
             filledBlocks[i][j] = filledBlocks[i-1][j];
@@ -627,6 +689,7 @@ void displayText(char str[], int x, int y, TTF_Font* font, int r, int g, int b) 
     float textW, textH;
     SDL_GetTextureSize(textTexture, &textW, &textH);
 
+    //set if set to centre
     if (x == -1) {
         x = (width >> 1) - (textW/2);
     }
@@ -642,6 +705,7 @@ void displayText(char str[], int x, int y, TTF_Font* font, int r, int g, int b) 
 bool displayShift = true;
 
 void setupStaticText() {
+    //board sizes 
     int widMinT = bWidthMin*TETROMINO_BLOCK_SIZE, widMaxT = bWidthMax*TETROMINO_BLOCK_SIZE;
     int higMinT = bHeightMin*TETROMINO_BLOCK_SIZE, higMaxT = bHeightMax*TETROMINO_BLOCK_SIZE;
 
@@ -660,11 +724,13 @@ void setupStaticText() {
 
     SDL_Surface *combined = SDL_CreateSurface(widMaxT+60+surface4->w, higMinT+(textHeight*3)+60, SDL_PIXELFORMAT_RGBA32);
 
+    //setup destination rects
     SDL_Rect dest1 = {(width >> 1) - (surface1->w >> 1) + 15, higMinT>>1, surface1->w, textHeight};
     SDL_Rect dest2 = {widMinT-180, higMinT+20, surface2->w, textHeight};
     SDL_Rect dest3 = {widMaxT+60, higMinT+20, surface3->w, textHeight};
     SDL_Rect dest4 = {widMaxT+60, higMinT+(textHeight << 1)+60, surface4->w, textHeight};
 
+    //copy to combined
     SDL_BlitSurface(surface1, NULL, combined, &dest1);
     SDL_BlitSurface(surface2, NULL, combined, &dest2);
     SDL_BlitSurface(surface3, NULL, combined, &dest3);
@@ -679,6 +745,7 @@ void setupStaticText() {
 
     SDL_GetTextureSize(staticText, &textW, &textH);
 
+    //destroy
     SDL_DestroySurface(surface1);
     SDL_DestroySurface(surface2);
     SDL_DestroySurface(surface3);
@@ -709,6 +776,7 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
     int halfTitleHeight = ((height >> 1) - (5 * TETROMINO_BLOCK_SIZE >> 1));
 
     if (titleCard) {
+        //change through colours
         if (now - lastChange >= 500) {
             if (currentMove == 6) {
                 currentMove = 0;
@@ -723,8 +791,11 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
             currentMove++;
             lastChange = now;
         }
+        //text
         displayText("Tribute by LKerr42", -1, (height >> 1) + (TETROMINO_BLOCK_SIZE << 1), globalFont, 255, 255, 255);
         displayText("Press Space to start", -1, 10, globalFont, 255, 255, 255);
+
+        //display blocks
         for (int countB = 0; countB < 6; countB++) {
             for (int Y = 0; Y < 5; Y++) {
                 for (int X = 0; X < 4; X++) {
@@ -752,6 +823,7 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
         char incompleteScore[7];
 
         if (now - lastFallTime >= 1000) {
+            //check if any are above a set block
             for (int a = 0; a < 4; a++) {
                 for (int b = 0; b < 4; b++) {
                     posX = tetArray[currentBlock].x+tetArray[currentBlock].blocks[a][b].x;
@@ -801,6 +873,7 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
                     }
                 }
 
+                //update score
                 if (linesCleared > 0) {
                     switch (linesCleared) {
                         case 1:
@@ -817,6 +890,7 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
                             break;
                     }
 
+                    //update string
                     sprintf(incompleteScore, "%d", score);
                     while (strlen(incompleteScore) != 7) {
                         prependChar(incompleteScore, '0');
@@ -826,10 +900,15 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
 
                 buildBoardTexture();
 
-                if (filledBlocks[1][5].v == true) {
-                    winning = false;
+                //lose condition
+                for (int m = 1; m < 11; m++) {
+                    if (filledBlocks[1][m].v == true) {
+                        winning = false;
+                        break;
+                    }
                 }
             } else {
+                //move down
                 if (toStop == false) {
                     tetArray[currentBlock].y += 1;
                 }  
@@ -840,6 +919,7 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
 
         renderBoard();
 
+        //display falling tetromino
         for (int y = 0; y < 4; y++) {
             for (int x = 0; x < 4; x++) {
                 if (tetArray[currentBlock].blocks[y][x].active) {
@@ -856,11 +936,31 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
             }
         }
 
-    } else if (titleCard == false) {
+        //display held tetromino
+        if (heldtet != -1) {
+            for (int k = 0; k < 4; k++) {
+                for (int l = 0; l < 4; l++) {
+                    if (shapes[heldtet][k][l] == 1) {
+                        posX = l;
+                        posY = k;
+                        SDL_FRect Hrect = {
+                            posX * TETROMINO_BLOCK_SIZE + (bWidthMin*TETROMINO_BLOCK_SIZE)-180 + (TETROMINO_BLOCK_SIZE << 1),
+                            posY * TETROMINO_BLOCK_SIZE + (bHeightMin*TETROMINO_BLOCK_SIZE)+70,
+                            TETROMINO_BLOCK_SIZE,
+                            TETROMINO_BLOCK_SIZE
+                        };
+                        displayBlock(Hrect, tetArray[heldtet].r, tetArray[heldtet].g, tetArray[heldtet].b, false);
+                    }
+                }
+            } 
+        }
+
+    } else if (titleCard == false) { //lose card
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);  // black, full alpha
         SDL_RenderClear(renderer);
         bool displayed = false;
         displayText("You Lost!", -1, -1, globalFont, 255, 255, 255);
+        displayText("Press space to start again", -1, (height >> 1) + 50, globalFontS, 255, 255, 255);
     }
 
     SDL_RenderPresent(renderer); // render everything
@@ -871,6 +971,7 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
 /* This function runs once at shutdown. */
 void SDL_AppQuit(void *appstate, SDL_AppResult result) {
     /* SDL will clean up the window/renderer for us. */
+    SDL_DestroyTexture(boardTexture);
     SDL_DestroyTexture(staticText);
     TTF_CloseFont(globalFont);
     TTF_Quit();
